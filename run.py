f7f4e75f211994fee19a51ab43b1db9e87ac6969
mattthughes <>
2023-12-02 13:00:10 +0000

    Added requesting input from user

--- run.py
+++ run.py
@@ -1,3 +1,31 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+
+import gspread
+from google.oauth2.service_account import Credentials
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('python-test')
+
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+    print("Please enter sales data from the last market")
+    print("Data shoul be six numbers seperated by commas")
+    print("Example 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here:")
+    print(f"The data provided is {data_str}")
+
+get_sales_data()
